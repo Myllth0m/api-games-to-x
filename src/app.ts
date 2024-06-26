5ad76b90039d2ef21bb2a476b7f5661fefb554a6
Milton Yama <miltondyama@gmail.com>
2024-06-26 14:12:41 -0400

    creating message on endpoint default

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,13 @@ connectDb()
 
 app.use(express.json())
 
+app.use('/', (req, res) => {
+  res.status(200).json({
+    message:
+      'API WORKING!! if u have any problem, pls send me on https://github.com/Myllth0m/api-games-to-x/issues',
+  })
+})
+
 app.use('/api/games', gameRoutes)
 
 export default app
